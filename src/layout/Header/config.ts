cacc1a8a2945f74c0aa234841c48a9431b1d5b67
stephenHe <hf597833184@163.com>
2024-02-04 17:15:11 +0800

    feat: 1:增加测试ts文件

---
+++ src/layout/Header/config.ts
@@ -0,0 +1,5 @@
+const a = (x: string, y: number) => {
+  return x + y + 1;
+};
+
+export { a };
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { a } from './config';
 
 import styles from './index.less';
 
 const Header = () => {
+  const AA = a("1", 2);
+
   return <div className={styles.headerWrapper}>
     <div>Header.....</div>
+    <div>{AA}</div>
   </div>;
 }
 export default React.memo(Header);
